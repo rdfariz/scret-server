87833fa0129c61852de67b7391c4ff0825277b25
rdfariz <rdfariz@student.telkomuniversity.ac.id>
2020-07-05 07:14:47 +0700

    initial deploy

---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(cors());
+app.use(router);
+
+io.on('connect', socket => {})
+io.on('connection', socket => {
+  let roomId
+
+  socket.on('join', data => {
+    roomId = data.roomId
+    if (roomId) {
+      socket.join(roomId)
+      const room = io.sockets.adapter.rooms[roomId];
+      socket.emit('join', room)
+      socket.broadcast.to(roomId).emit('bc_join', room)
+    }
+  })
+
+  socket.on('update_code', data => {
+    if (roomId) {
+      const { code } = data
+      socket.emit('update_code', { code })
+      socket.broadcast.to(roomId).emit('update_code', { code })
+    }
+  })
+
+  socket.on('disconnect', function() {
+    if (roomId) {
+      const room = io.sockets.adapter.rooms[roomId];
+      socket.emit('join', room)
+      socket.broadcast.to(roomId).emit('bc_join', room)
+    }
+  })
+})
+
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
